b00a6c52a3edfeb5f804ea42fbe43011bdafa215
sahaorchid <70687710+sahaorchid@users.noreply.github.com>
2021-11-24 18:04:11 +0530

    n b
    nb

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const apiRoutes = require('./routes/users/index')
 const todoRoutes = require('./routes/userTodos/index')
+const authRouter = require('./routes/auth/index')
 
 const app = express()
 app.use(express.json())
 app.use('/userapi',apiRoutes)
 app.use('/todoapi',todoRoutes)
+app.use('/auth',authRouter)
 
 app.listen(3000,()=>{
     console.log("server started")
